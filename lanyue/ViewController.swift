0a6fc565ac0f9e3d3b9531aab0379a4003b20e3b
user <user@luyidemac-mini.local>
2017-10-23 19:36:27 +0800

    webview

--- lanyue/ViewController.swift
+++ lanyue/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate {
 
+    @IBOutlet weak var gameView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(self.webView)
+        view.addSubview(self.progressView)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // webView
+    lazy var webView : WKWebView = {
+        /// 设置访问的URL
+        let url = NSURL(string: "http://106.75.29.38:8183/youxitest.php")
+        /// 根据URL创建请求
+        let requst = NSURLRequest(url: url! as URL)
+        /// 设置代理
+        //        web.uiDelegate = self
+        gameView.navigationDelegate = self
+        /// WKWebView加载请求
+        gameView.load(requst as URLRequest)
+        
+        return gameView
+    }()
+    
+    
+    // 进度条
+    lazy var progressView:UIProgressView = {
+        let progress = UIProgressView()
+        progress.progressTintColor = UIColor.red
+        progress.trackTintColor = .clear
+        return progress
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.progressView.frame = CGRect(x:0,y:64,width:self.view.frame.size.width,height:2)
+        self.progressView.isHidden = false
+        UIView.animate(withDuration: 1.0) {
+            self.progressView.progress = 0.0
+        }
+    }
+    
 }
 
